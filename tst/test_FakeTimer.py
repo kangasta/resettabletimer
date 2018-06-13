59ce23ec91befa7547b0ac25259c660ec82348bd
Toni Kangas <toni.ar.kangas@gmail.com>
2018-06-13 22:24:03 +0300

    Add fake timer time pass tests

--- tst/test_FakeTimer.py
+++ tst/test_FakeTimer.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
 from FakeTimer import FakeTimer
 
@@ -9,10 +10,21 @@ class FakeTimerTest(TestCase):
 		with self.assertRaises(RuntimeError):
 			t.start()
 
-
 	def test_cannot_be_restarted_after_cancel(self):
 		t = FakeTimer(0, lambda : None)
 		t.start()
 		t.cancel()
 		with self.assertRaises(RuntimeError):
 			t.start()
+
+	def test_calls_function_after_time_has_passed(self):
+		m = Mock(return_value=None)
+		t = FakeTimer(5, m)
+		t.pass_time(5)
+		m.assert_called_once_with()
+
+	def test_does_not_call_function_before_time_has_passed(self):
+		m = Mock(return_value=None)
+		t = FakeTimer(5, m)
+		t.pass_time(3)
+		m.assert_not_called()
