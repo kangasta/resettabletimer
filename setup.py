dfe3bb1efefd7822fe0bc428c1ba98af3acf2a18
Toni Kangas <toni.ar.kangas@gmail.com>
2021-11-30 22:28:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
-	long_description = f.read()
+    long_description = f.read()
 
-setuptools.setup(
-	name='resettabletimer',
-	version='0.7.0',
-	author='Toni Kangas',
-	description='Wrapper for threading.Timer to allow resetting',
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/kangasta/resettabletimer",
-	packages=setuptools.find_packages(),
-	classifiers=(
-		"Programming Language :: Python :: 2.7",
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	)
+setup(
+    name='resettabletimer',
+    version='0.7.0',
+    author='Toni Kangas',
+    description='Wrapper for threading.Timer to allow resetting',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kangasta/resettabletimer",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
 )
