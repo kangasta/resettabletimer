c2cc1c4359b2c6e00b8e0815fba15b36dc74c427
Toni Kangas <toni.ar.kangas@gmail.com>
2021-11-30 22:58:42 +0200

    Format code according to PEP 8

--- resettabletimer/_resettabletimer.py
+++ resettabletimer/_resettabletimer.py
@@ -1,30 +1,35 @@
 from threading import Timer
 
+
 class ResettableTimer(object):
-	def __init__(self, time, function, args=[], kwargs={}):
-		self.__time = time
-		self.__function = function
-		self.__args = args
-		self.__kwargs = kwargs
-		self.__set()
-		self.__running = False
+    def __init__(self, time, function, args=[], kwargs={}):
+        self.__time = time
+        self.__function = function
+        self.__args = args
+        self.__kwargs = kwargs
+        self.__set()
+        self.__running = False
 
-	def __set(self):
-		self.__timer = Timer(self.__time, self.__function, self.__args, self.__kwargs)
+    def __set(self):
+        self.__timer = Timer(
+            self.__time,
+            self.__function,
+            self.__args,
+            self.__kwargs)
 
-	def start(self):
-		self.__running = True
-		self.__timer.start()
+    def start(self):
+        self.__running = True
+        self.__timer.start()
 
-	def cancel(self):
-		self.__running = False
-		self.__timer.cancel()
+    def cancel(self):
+        self.__running = False
+        self.__timer.cancel()
 
-	def reset(self, start=False):
-		if self.__running:
-			self.__timer.cancel()
+    def reset(self, start=False):
+        if self.__running:
+            self.__timer.cancel()
 
-		self.__set()
+        self.__set()
 
-		if self.__running or start:
-			self.start()
+        if self.__running or start:
+            self.start()
