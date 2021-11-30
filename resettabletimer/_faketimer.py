1620ab1baf1b8b7e4b2e9de7993160e7f4eb2817
Toni Kangas <toni.ar.kangas@gmail.com>
2021-11-30 23:40:20 +0200

    Use _ for private properties instead of __

--- resettabletimer/_faketimer.py
+++ resettabletimer/_faketimer.py
@@ -1,29 +1,29 @@
 class FakeTimer(object):
-    def __init__(self, time, function, args=[], kwargs={}):
-        self.__time = time
-        self.__function = function
-        self.__args = args
-        self.__kwargs = kwargs
-        self.__canceled = False
-        self.__started = False
-        self.__passed = 0
+    def __init__(self, time, function, args=None, kwargs=None):
+        self._time = time
+        self._function = function
+        self._args = args if args is not None else []
+        self._kwargs = kwargs if kwargs is not None else {}
+        self._canceled = False
+        self._started = False
+        self._passed = 0
 
     def start(self):
-        if self.__started:
+        if self._started:
             raise RuntimeError("threads can only be started once")
-        self.__started = True
-        self.__passed = 0
+        self._started = True
+        self._passed = 0
 
     def cancel(self):
-        self.__canceled = True
+        self._canceled = True
 
     def reset(self):
-        self.__passed = 0
-        if self.__canceled:
-            self.__started = False
-        self.__canceled = False
+        self._passed = 0
+        if self._canceled:
+            self._started = False
+        self._canceled = False
 
     def pass_time(self, time):
-        self.__passed += time
-        if not self.__canceled and self.__started and self.__passed >= self.__time:
-            self.__function(*self.__args, **self.__kwargs)
+        self._passed += time
+        if not self._canceled and self._started and self._passed >= self._time:
+            self._function(*self._args, **self._kwargs)
--- resettabletimer/_resettabletimer.py
+++ resettabletimer/_resettabletimer.py
@@ -2,34 +2,34 @@ from threading import Timer
 
 
 class ResettableTimer(object):
-    def __init__(self, time, function, args=[], kwargs={}):
-        self.__time = time
-        self.__function = function
-        self.__args = args
-        self.__kwargs = kwargs
-        self.__set()
-        self.__running = False
+    def __init__(self, time, function, args=None, kwargs=None):
+        self._time = time
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+        self._set()
+        self._running = False
 
-    def __set(self):
-        self.__timer = Timer(
-            self.__time,
-            self.__function,
-            self.__args,
-            self.__kwargs)
+    def _set(self):
+        self._timer = Timer(
+            self._time,
+            self._function,
+            self._args,
+            self._kwargs)
 
     def start(self):
-        self.__running = True
-        self.__timer.start()
+        self._running = True
+        self._timer.start()
 
     def cancel(self):
-        self.__running = False
-        self.__timer.cancel()
+        self._running = False
+        self._timer.cancel()
 
     def reset(self, start=False):
-        if self.__running:
-            self.__timer.cancel()
+        if self._running:
+            self._timer.cancel()
 
-        self.__set()
+        self._set()
 
-        if self.__running or start:
+        if self._running or start:
             self.start()
